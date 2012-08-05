0edcf64ca41b10021a1bcc432efa397dcedea9e2
Hezekiah M. Carty <hez@0ok.org>
2012-08-05 18:25:56 -0400

    Remove the 'pj_' prefix on function names since we're in a module

--- src/proj4.ml
+++ src/proj4.ml
@@ -3,11 +3,11 @@
 (** Projection type *)
 type t
 
-external pj_init_plus : string -> t = "ml_pj_init_plus"
-external pj_transform : t -> t -> float array -> float array ->
+external init_plus : string -> t = "ml_pj_init_plus"
+external transform : t -> t -> float array -> float array ->
   float array * float array = "ml_pj_transform"
-external pj_is_latlong : t -> bool = "ml_pj_is_latlong"
-external pj_is_geocent : t -> bool = "ml_pj_is_geocent"
-external pj_get_def : t -> string = "ml_pj_get_def"
-external pj_latlong_from_proj : t -> t = "ml_pj_latlong_from_proj"
+external is_latlong : t -> bool = "ml_pj_is_latlong"
+external is_geocent : t -> bool = "ml_pj_is_geocent"
+external get_def : t -> string = "ml_pj_get_def"
+external latlong_from_proj : t -> t = "ml_pj_latlong_from_proj"
 
--- src/proj4.mli
+++ src/proj4.mli
@@ -1,8 +1,8 @@
 type t
-val pj_init_plus : string -> t
-val pj_transform :
+val init_plus : string -> t
+val transform :
   t -> t -> float array -> float array -> float array * float array
-val pj_is_latlong : t -> bool
-val pj_is_geocent : t -> bool
-val pj_get_def : t -> string
-val pj_latlong_from_proj : t -> t
+val is_latlong : t -> bool
+val is_geocent : t -> bool
+val get_def : t -> string
+val latlong_from_proj : t -> t
